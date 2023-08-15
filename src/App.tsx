6918969e37949f79a4cc493e85dc52a9a9d44478
rentezzy <ren2ren4ik@gmail.com>
2023-08-15 21:49:15 +0300

    Create app

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div className="container">
+      <p className={styles.asd}>asdasd</p>
+    </div>
+  );
+}
+
+export default App;
